dfe153012d230b7d0b6069eaf099c874370d31b4
Sai Kiran Challa <schalla@adobe.com>
2023-05-24 08:42:52 -0600

    Testing Hooks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! From DotNet");
 Console.WriteLine("Hope you're learning something new!");
+Console.WriteLine("Hope you're learning something new!");
